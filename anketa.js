6ebb844b4b58bba54bf676433586f6961822082d
yakovlEva99 <evulya_yakovlev@mail.ru>
2022-09-01 23:06:01 +0700

    new js

---
+++ anketa.js
@@ -0,0 +1,5 @@
+var firstName = prompt("Введите имя");
+var secondName = prompt("Введите фамилию");
+console.log("Пользователь:" + " " + firstName + " " + secondName);
+var age = prompt("Введите возраст");
+console.log("Возраст:" + " " + age);
